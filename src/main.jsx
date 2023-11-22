b9f52ed153a96865654c6b89b70f21134a1837cb
IGamero <ivan.gamero.olivares@gmail.com>
2023-11-22 05:54:48 +0100

    ª

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "../css/normalize.css"
+import "../src/assets/css/normalize.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Components
